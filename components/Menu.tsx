e6954e438ef0b512b3d4ca89e6e71525c7761ca8
alkhatera <alkhateralaa@outlook.com>
2025-06-28 11:44:36 +0300

    improve menu styling

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,7 +1,12 @@
 import { useFocusContext } from '@/ts/contexts/FocusContext';
+import { LinearGradient } from 'expo-linear-gradient';
 import { useMemo } from 'react';
 import { TVFocusGuideView, useTVEventHandler, View } from 'react-native';
+import Animated, { useAnimatedStyle, withSpring, withTiming } from 'react-native-reanimated';
 import MenuItem from './MenuItem';
+
+const AnimatedLinearGradient = Animated.createAnimatedComponent(LinearGradient);
+const AnimatedTVFocusGuideView = Animated.createAnimatedComponent(TVFocusGuideView);
 
 function Menu() {
 	const { focusedComponent, changeFocus } = useFocusContext();
@@ -27,45 +32,73 @@ function Menu() {
 		}
 	});
 
+	const animatedWidth = useAnimatedStyle(() => {
+		return {
+			width: withSpring(focusedComponent?.name === 'menu' ? 300 : 100, {
+				damping: 15,
+				stiffness: 120,
+				mass: 0.8,
+			}),
+		};
+	});
+
+	const contentWidth = useAnimatedStyle(() => {
+		return {
+			width: withSpring(focusedComponent?.name === 'menu' ? 200 : 100, {
+				damping: 15,
+				stiffness: 120,
+				mass: 0.8,
+			}),
+		};
+	});
+
 	return (
-		<TVFocusGuideView
+		<AnimatedLinearGradient
+			colors={[
+				'rgba(0,0,0,0.75)', // solid start
+				'rgba(0,0,0,0.75)', // solid until 200px
+				'rgba(0,0,0,0.6)',
+				'rgba(0,0,0,0.4)',
+				'rgba(0,0,0,0.2)',
+				'rgba(0,0,0,0.1)',
+				'transparent',
+			]}
+			locations={[0, 200 / 300, 220 / 300, 240 / 300, 260 / 300, 280 / 300, 1]}
+			start={{ x: 0, y: 0 }} // Top
+			end={{ x: 1, y: 0 }}
 			style={[
 				{
-					backgroundColor: 'rgba(0,0,0,0.75)',
-					width: 100,
 					height: '100%',
 					position: 'absolute',
 					top: 0,
+					left: 0,
+					bottom: 0,
 					zIndex: 1,
-					left: -200,
-					transform: [{ translateX: 200 }],
 				},
-				focusedComponent?.name === 'menu'
-					? {
-							width: 200,
-					  }
-					: null,
+				animatedWidth,
 			]}
 		>
-			{/* Profile */}
-			<View
-				style={{
-					backgroundColor: '#808080',
-					width: 50,
-					height: 50,
-					top: 30,
-					left: '50%',
-					transform: [{ translateX: -25 }],
-					borderRadius: 30,
-					marginBottom: 110,
-				}}
-			/>
+			<AnimatedTVFocusGuideView autoFocus trapFocusRight style={[{ flex: 1 }, contentWidth]}>
+				{/* Profile */}
+				<View
+					style={{
+						backgroundColor: '#808080',
+						width: 50,
+						height: 50,
+						top: 30,
+						left: '50%',
+						transform: [{ translateX: -25 }],
+						borderRadius: 30,
+						marginBottom: 110,
+					}}
+				/>
 
-			{/* Menu Item */}
-			{menuItems.map((_, index) => (
-				<MenuItem key={index} index={index} />
-			))}
-		</TVFocusGuideView>
+				{/* Menu Item */}
+				{menuItems.map((_, index) => (
+					<MenuItem key={index} index={index} />
+				))}
+			</AnimatedTVFocusGuideView>
+		</AnimatedLinearGradient>
 	);
 }
 
