712ef3ac22bdbb7dcaa59e81e8817a9123e244bd
alkhatera <alkhateralaa@outlook.com>
2025-06-29 01:41:08 +0300

    chore: add mock api data if no key is available

--- hooks/api/useFetchVideos.ts
+++ hooks/api/useFetchVideos.ts
@@ -1,3 +1,4 @@
+import { MOCK_DATA } from '@/ts/data';
 import useVideosStore from '@/ts/zustand/useVideosStore';
 import { useQuery } from '@tanstack/react-query';
 
@@ -8,15 +9,23 @@ function useFetchVideos() {
 		queryKey: ['videos'],
 		queryFn: async () => {
 			try {
+				if (!process.env.PIXABAY_API_KEY) {
+					setTimeout(() => {
+						console.warn('No API key set for Pixabay. Using mock data instead.');
+					}, 1000);
+					setVideos(MOCK_DATA); // Set mock data if API key is not set
+					return MOCK_DATA; // Return mock data immediately if API key is not set
+				}
 				const response = await fetch(`https://pixabay.com/api/videos/?key=${process.env.PIXABAY_API_KEY}&q=adventure&per_page=10`);
 				if (!response.ok) {
 					throw new Error('Network response was not ok');
 				}
 				const data = await response.json();
 				setVideos(data.hits);
-				return data;
+				return data.hits;
 			} catch (error) {
 				console.error('Error fetching videos:', error);
+			} finally {
 			}
 		},
 	});
