0682656d268f580e6fc51952e3d25b014b41acd8
alkhatera <alkhateralaa@outlook.com>
2025-06-28 21:14:26 +0300

    small fix

--- hooks/useDebouncedTVEventHandler.ts
+++ hooks/useDebouncedTVEventHandler.ts
@@ -2,22 +2,26 @@ import { useEffect, useRef } from 'react';
 import { HWEvent, useTVEventHandler } from 'react-native';
 import { useIsFocused } from '@react-navigation/native';
 
-function useDebouncedTVEventHandler(callback: (event: HWEvent) => void, delay = 1000) {
+function useDebouncedTVEventHandler(callback: (event: HWEvent) => void, delay = 300, enabled = true) {
 	const isFocused = useIsFocused();
-
 	const lastCall = useRef(0);
 	const callbackRef = useRef(callback);
+	const delayRef = useRef(delay);
 
-	// Keep callback up-to-date
+	// Keep callback and delay up-to-date
 	useEffect(() => {
 		callbackRef.current = callback;
 	}, [callback]);
 
+	useEffect(() => {
+		delayRef.current = delay;
+	}, [delay]);
+
 	useTVEventHandler((evt) => {
-		if (!isFocused) return; // Ignore events if the screen is not focused
+		if (!enabled || !isFocused || !evt) return;
 
 		const now = Date.now();
-		if (now - lastCall.current < delay) return;
+		if (now - lastCall.current < delayRef.current) return;
 
 		lastCall.current = now;
 		callbackRef.current(evt);
